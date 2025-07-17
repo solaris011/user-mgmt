6625a15956a4ebcaa3428cd1692b3714c8229051
Saul Flores <solaris0250@gmail.com>
2025-07-17 06:37:55 -0600

    User details v1

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -2,14 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import user from "../images/user.png";
 
-const CardUser = (props) => {
-   const { id, name, email } = props.user;
-
+const UserCard = (props) => {
    return (
-      <div className="item" style={{ 
-         display: 'flex', 
-         alignItems: 'center', 
-         position: 'relative', 
+      <div className="item" style={{
+         display: 'flex',
+         alignItems: 'center',
+         position: 'relative',
          backgroundColor: props.backgroundColor || '#ffffff',
          padding: '10px',
          borderRadius: '5px',
@@ -17,17 +15,17 @@ const CardUser = (props) => {
       }}>
          <img className="ui avatar image" src={user} alt="user" style={{ width: '50px', height: '50px', marginRight: '10px' }} />
          <div className="content" style={{ flex: 1 }}>
-            <Link to={`/user/${id}`} style={{ textDecoration: 'none', color: 'black' }}>
-               <div className="header" style={{ fontWeight: 'bold' }}>{name}</div>
+            <Link to={`/user/${props.user.id}`} style={{ textDecoration: 'none', color: 'black' }}>
+               <div className="header" style={{ fontWeight: 'bold' }}>{props.user.name}</div>
             </Link>
-            <div>{email}</div>
+            <div>{props.user.email}</div>
          </div>
          <i 
             className="trash alternate outline icon large" style={{ color: 'red', cursor: 'pointer', position: 'absolute', right: '15px' }}
-            onClick={() => props.clickHandler(id)}
-         ></i>
+            onClick={() => props.clickHandler(props.user.id)}
+         ></i> 
       </div>
    );
 }
 
-export default CardUser;
+export default UserCard;
